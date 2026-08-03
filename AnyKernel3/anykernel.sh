bb2746cf076166134a1c25fabfd29ded48f7c5c4
SUJITH <141490624+sujithmax@users.noreply.github.com>
2026-08-03 18:05:49 +0000

    modified anykernel script

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=BrickedKernel by Stability
+kernel.string=nothing to say just flash and go..
 do.devicecheck=0
 do.modules=0
 do.systemless=1
