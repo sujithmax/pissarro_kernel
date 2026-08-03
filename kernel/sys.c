8b78cb0b5e736b2001a2896a2cd3832cc784cf2a
SUJITH <141490624+sujithmax@users.noreply.github.com>
2026-08-03 11:00:30 +0000

    added ebpf

--- kernel/sys.c
+++ kernel/sys.c
@@ -1205,14 +1205,15 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
-    for (i = 0; i < ARRAY_SIZE(fake_comm); i++) {
-		if (!strncmp(comm, fake_comm[i].name, fake_comm[i].len)) {
-			strscpy(tmp.release, "5.10.253",
-				sizeof(tmp.release));
-			break;
+	if (!strncmp(current->comm, "bpfloader", 9) ||
+	    !strncmp(current->comm, "netbpfload", 10) ||
+	    !strncmp(current->comm, "netd", 4)) {
+		if (current_uid().val == 0) {
+			strcpy(tmp.release, "5.10.253");
+			pr_debug("fake uname: %s/%d release=%s\n",
+				 current->comm, current->pid, tmp.release);
 		}
 	}
-
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;
